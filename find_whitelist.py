5d005e81612523cf065384e8d9153aae53617f26
kbj804 <rain1344@naver.com>
2018-09-07 13:01:01 +0900

    return source_url

--- find_whitelist.py
+++ find_whitelist.py
@@ -2,28 +2,42 @@
 
 import pymysql
 
-find_source_id="select harmful_source_id from collected_url where url_id=%s"
+find_source_id="select harmful_source_id from collected_url where url_id= %s"
+verify_whitelist="select harmful from collected_url where url_id=%s"
 
 
-def find_source(id):  # return generate urls
-    connection = pymysql.connect(host='localhost', user='bj', password='1234', db='url_db', charset='utf8')
+
+def extract_source(id):  #
+    #connection = pymysql.connect(host='localhost', user='bj', password='1234', db='url_db', charset='utf8')
     with connection.cursor() as curs:
         curs.execute(find_source_id, id)
-        harmful_source_id = curs.fetchone()
-        print(harmful_source_id)
+        for harmful_source_id in curs.fetchone(): # fetch -> dictionary
+            if harmful_source_id == '0' :  # null
+                connection.close()
+                print("source id: ", id)
+                return id
+            else:
+                print(harmful_source_id)
+                id2 = extract_source(harmful_source_id)
+                return id2
 
-        if harmful_source_id is None: # null
-            connection.close()
-            return id
-        elif harmful_source_id is not None:
-            find_source(harmful_source_id)
 
+def verify(id):
+    #connection = pymysql.connect(host='localhost', user='bj', password='1234', db='url_db', charset='utf8')
+    with connection.cursor() as curs:
+        curs.execute(verify_whitelist, id)
+        for harmful in curs.fetchone(): # if id is in Whitelist, extract source id.
+            if harmful == 3 :
+                connection.close()
+                print("start id: ", id)
+                source_url = extract_source(id)
+                return 1
+            else:
+                print("It is harmful URL. ")
+                return 0
 
-
-def exe(id):
-        source_id = find_source(id)
-        print("source")
 
 
 if __name__ == "__main__":
-    exe(25253)  # input url_id
+    connection = pymysql.connect(host='localhost', user='bj', password='1234', db='url_db', charset='utf8')
+    verify(16085)  # input url_id
