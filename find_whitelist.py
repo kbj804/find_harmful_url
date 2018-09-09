f4a4e8cd65a4b486f76d350e1058899640e8b973
kbj804 <rain1344@naver.com>
2018-09-09 17:40:36 +0900

    update white_list ca

--- find_whitelist.py
+++ find_whitelist.py
@@ -5,8 +5,12 @@ import pymysql
 find_source_id="select harmful_source_id from collected_url where url_id= %s"
 verify_whitelist="select harmful from collected_url where url_id=%s"
 
-update_node_whitelist = "UPDATE collected_url SET harmful='3' WHERE url_id= %s"
+update_node_whitelist = "UPDATE collected_url SET harmful='3' WHERE url_id= %s and harmful!='3'"
 find_child_url="select child_id from url_relation where parent_id=%s"
+
+load_whitelist ="select url_id from collected_url where harmful='3' and white_visit=0"
+add_white_visit="update collected_url set white_visit = 1 where url_id=%s"
+
 
 def extract_source(id):  #
     #connection = pymysql.connect(host='localhost', user='bj', password='1234', db='url_db', charset='utf8')
@@ -24,12 +28,11 @@ def extract_source(id):  #
                 print(child_group)
                 for child_id in child_group: ############################################ Need to solution duplication probelm
                     print("UPDATE WHITE LIST: ", child_id[0])
-                    wtf=curs.execute(update_node_whitelist,child_id[0])
-                    print(wtf)
+                    curs.execute(update_node_whitelist,child_id[0])
 
                 # update child_node
                 # curs.execute(update_node_whitelist, id)
-
+                curs.close()
                 id2 = extract_source(harmful_source_id)
                 return id2
 
@@ -40,8 +43,9 @@ def verify(id):
     with connection.cursor() as curs:
         curs.execute(verify_whitelist, id)
         for harmful in curs.fetchone(): # if id is in Whitelist, extract source id.
+            curs.close()
             if harmful == 3 :
-                #connection.close()
+                curs.execute(add_white_visit,id)
                 print("verify id: ", id)
                 return 1
             else:
@@ -49,17 +53,26 @@ def verify(id):
                 return 0
 
 
+def generate_url():
+    with connection.cursor() as curs:
+        curs.execute(load_whitelist)
+        white_list = curs.fetchall()
+        curs.close()
+        return white_list
+
+
 
 if __name__ == "__main__":
     connection = pymysql.connect(host='localhost', user='bj', password='1234', db='url_db', charset='utf8')
-    url_id = 16085
-    first_value = verify(url_id)  # value = 0: No Whitelist / value = 1: Whitelist
-    if first_value == 1:
-        source_url = extract_source(url_id)
-        last_value = verify(source_url)
-        if last_value == 1 :
-            connection.commit()
-            connection.close()
-            print("Input All White list. ")
-    else:
-        ("else")
+    white_list = generate_url()
+    for url_id in white_list:
+        first_value = verify(url_id[0])  # value = 0: No Whitelist / value = 1: Whitelist
+        if first_value == 1:
+            source_url = extract_source(url_id[0])
+            last_value = verify(source_url)
+            if last_value == 1 :
+                print("Input All White list. ")
+        else:
+            ("It is no whitelist...")
+    connection.commit()
+    connection.close()
